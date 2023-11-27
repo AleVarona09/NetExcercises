8d71198f9760d474f892e8fc6dba039161df8405
Alejandro Varona <avaronat@gmail.com>
2023-11-27 15:24:26 -0500

    Fix Bug in line 7, the method used to start comparing from position 1, now it starts at position 0 of the array.

--- NetExcercises/Program.cs
+++ NetExcercises/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+int Solution(int[] numbers)
+{
+    int small = numbers[0]; // before int small = 0;
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        if (numbers[i] < small) { 
+            small = numbers[i];
+        }
+    }
+
+    return small;
+}
+
+
+int[] numbers = {1, -10, -2, 1, -1, 2 , -9};
+
+int value = Solution(numbers);
+Console.WriteLine("The smallest number in the array is: " + value + "\n");
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine("---------------------------------");
+
+
+
+
+
+
+
+
+Console.ReadLine();
+
